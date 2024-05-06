0a2152f173e7fb050e7b819619ac920ff807e0d5
Elanis <elanis@hotmail.com>
2024-05-06 14:01:20 +0200

    Create Test

--- L3WebApi.WebApi.Tests/GamesController.Test.cs
+++ L3WebApi.WebApi.Tests/GamesController.Test.cs
@@ -1,8 +1,10 @@
 ﻿using FluentAssertions;
 using L3WebApi.Common.DTO;
+using L3WebApi.Common.Requests;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using System.Net;
+using System.Text;
 using System.Text.Json;
 
 namespace L3WebApi.WebApi.Tests {
@@ -36,7 +38,7 @@ namespace L3WebApi.WebApi.Tests {
 		[Theory]
 		[InlineData(1, HttpStatusCode.OK)]
 		[InlineData(2, HttpStatusCode.NotFound)]
-		public async void ShouldGetReleventHttpCode_GET_By_Id(int id, HttpStatusCode code) {
+		public async void ShouldGetReleventHttpCode_GET_ById(int id, HttpStatusCode code) {
 			var response = await client.GetAsync($"/api/Games/{id}");
 
 			response.StatusCode.Should().Be(code);
@@ -49,6 +51,24 @@ namespace L3WebApi.WebApi.Tests {
 
 				data.Should().NotBeNull();
 			}
+		}
+
+		[Fact]
+		public async void ShouldGet201_POST_Create() {
+			var game = new GameCreationRequest {
+				Name = "test",
+				Description = "test de description plutôt longue",
+				Logo = "http://domain.tld/logo.png"
+			};
+
+			var content = new StringContent(
+				JsonSerializer.Serialize(game),
+				Encoding.UTF8,
+				"application/json"
+			);
+
+			var response = await client.PostAsync("/api/Games/", content);
+			response.StatusCode.Should().Be(HttpStatusCode.Created);
 		}
 	}
 }
