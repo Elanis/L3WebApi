68200d192b652ec4d04ab2ee035a144181971c14
Elanis <elanis@hotmail.com>
2024-05-31 11:40:38 +0200

    Setup postgres

--- L3WebApi.DataAccess/Implementations/GamesDataAccess.cs
+++ L3WebApi.DataAccess/Implementations/GamesDataAccess.cs
@@ -4,47 +4,43 @@ using L3WebApi.DataAccess.Interfaces;
 
 namespace L3WebApi.DataAccess.Implementations {
 	public class GamesDataAccess : IGamesDataAccess {
-		private readonly static List<GameDao> AllGames = [
-			new GameDao {
-				Id = 1,
-				Name = "Zelda",
-				Description = "Description",
-				Logo = "logo.png"
-			}
-		];
+		private readonly GameContext _context;
+		public GamesDataAccess(GameContext context) {
+			this._context = context;
+		}
 
 		public async Task<IEnumerable<GameDao>> GetGames() {
-			return AllGames;
+			return _context.Games;
 		}
 
 		public async Task<GameDao?> GetGameById(int id) {
-			return AllGames.FirstOrDefault(x => x.Id == id);
+			return _context.Games.FirstOrDefault(x => x.Id == id);
 		}
 
 		public async Task<IEnumerable<GameDao>> SearchByName(string name) {
-			return AllGames.Where(x => x.Name.Contains(name));
+			return _context.Games.Where(x => x.Name.Contains(name));
 		}
 
 		public async Task<GameDao> Create(GameCreationRequest request) {
-			var id = AllGames.Max(x => x.Id) + 1;
-
-			AllGames.Add(new GameDao {
-				Id = id,
+			var newGame = _context.Games.Add(new GameDao {
 				Name = request.Name,
 				Description = request.Description,
 				Logo = request.Logo,
 			});
 
-			return await GetGameById(id);
+			_context.SaveChanges();
+
+			return await GetGameById(newGame.Entity.Id);
 		}
 
-		public async Task SaveChanges() {
-			// Rien
+		public Task SaveChanges() {
+			return _context.SaveChangesAsync();
 		}
 
 		public async Task Remove(int id) {
 			var game = await GetGameById(id);
-			AllGames.Remove(game);
+			_context.Games.Remove(game);
+			_context.SaveChanges();
 		}
 	}
 }
